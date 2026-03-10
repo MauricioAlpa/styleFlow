aaf981255a3d0d3d20623ebd198115d8b5b157ac
igopereira1 <igopereira1@gmail.com>
2026-03-10 10:30:59 -0300

    fix: corrigindo estrutura de métodos e imports do SupplierController

--- src/Controllers/SupplierController.js
+++ src/Controllers/SupplierController.js
@@ -1,4 +1,6 @@
-export class Suppliers {
+import { pool as db } from "../Config/database.js";
+
+export class SupplierController {
   async adicionarSupplier({ fornecedor }) {
     const query = ` INSERT INTO suppliers (nome_fantasia, cnpj, contato) VALUES ($1, $2, $3) RETURNING id, nome_fantasia, cnpj, contato `;
     const values = [
@@ -13,41 +15,41 @@ export class Suppliers {
       return error.message;
     }
   }
-}
 
-async function listarSuppliers() {
-  const query = `SELECT * FROM suppliers;`;
-  try {
-    const result = await db.query(query);
-    return result.rows;
-  } catch (error) {
-    return error.message;
+  async listarSuppliers() {
+    const query = `SELECT * FROM suppliers;`;
+    try {
+      const result = await db.query(query);
+      return result.rows;
+    } catch (error) {
+      return error.message;
+    }
   }
-}
 
-async function deletarSupplier({ id }) {
-  const query = `DELETE FROM suppliers WHERE id = $1 RETURNING id`;
-  const values = [id];
-  try {
-    const result = await db.query(query, values);
-    return result.rows[0];
-  } catch (error) {
-    return error.message;
+  async deletarSupplier(id) {
+    const query = `DELETE FROM suppliers WHERE id = $1 RETURNING id`;
+    const values = [id];
+    try {
+      const result = await db.query(query, values);
+      return result.rows[0];
+    } catch (error) {
+      return error.message;
+    }
   }
-}
 
-async function atualizarSupplier({ fornecedor }) {
-  const query = `UPDATE suppliers SET nome_fantasia = $1, cnpj = $2, contato = $3 WHERE id = $4 RETURNING id, nome_fantasia, cnpj, contato`;
-  const value = [
-    fornecedor.nome_fantasia,
-    fornecedor.cnpj,
-    fornecedor.contato,
-    fornecedor.id,
-  ];
-  try {
-    const result = await db.query(query, value);
-    return result.rows[0];
-  } catch (error) {
-    return error.message;
+  async atualizarSupplier({ fornecedor }) {
+    const query = `UPDATE suppliers SET nome_fantasia = $1, cnpj = $2, contato = $3 WHERE id = $4 RETURNING id, nome_fantasia, cnpj, contato`;
+    const value = [
+      fornecedor.nome_fantasia,
+      fornecedor.cnpj,
+      fornecedor.contato,
+      fornecedor.id,
+    ];
+    try {
+      const result = await db.query(query, value);
+      return result.rows[0];
+    } catch (error) {
+      return error.message;
+    }
   }
 }
